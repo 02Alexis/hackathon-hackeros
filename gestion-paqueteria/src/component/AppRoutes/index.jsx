287c377a19d7d45542f7a1d15daa86dc41f9caa6
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-09-09 15:14:00 -0500

    First version SendForm component

--- gestion-paqueteria/src/component/AppRoutes/index.jsx
+++ gestion-paqueteria/src/component/AppRoutes/index.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Navbar from '../Navbar'
+import SentForm from '../../pages/SentForm'
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Navbar />} />
+        <Route path='sent-form' element={<SentForm />} />
       </Routes>
     </div>
   )
